146e42cdaadffd93cc666799145bff18f429d348
lumocolor98800 <manon.pc@hotmail.fr>
2019-02-06 15:35:07 +0100

    correction du script rules

--- rules.sh
+++ rules.sh
@@ -2,20 +2,20 @@
 # echappement ^ $ \ | { } [ ] ( ) ? # ! + * .
 
 #pour la date : \[\d{6}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]
-regex_date = \[\d{6}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]
+REGEX_DATE = \[\d{6}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]
 
 #minium 3 de taille ?
-regex_protocol = \S+
+REGEX_PROTOCOL = \S+
 
-regex_ip = ([0-9]{1,3}\.){3}[0-9]{1,3}
+REGEX_IP = ([0-9]{1,3}\.){3}[0-9]{1,3}
 
-regex_port = [0-9]{1,5}
+REGEX_PORT = [0-9]{1,5}
 
-regex_message = .*\n
+REGEX_MESSAGE = .*\n
 
-message = "Connection to server" 
+MESSAGE = "Connection to server" 
 
-tab-rule = (regex_date \s+ regex_protocol \s+ regex_ip \s+ regex_ip \s+ regex_port \s+ regex_port \s+ message )
+tab-rule = ($REGEX_DATE \s+ $REGEX_PROTOCOL \s+ $REGEX_PROTOCOL \s+ $REGEX_IP \s+ $REGEX_IP \s+ $REGEX_PORT \s+ $REGEX_PORT \s+ $MESSAGE )
 
 
 for i in ${tab-rule[@]}; 
